7ecb88451f551518a63c8119085f9470b164b951
octavio <vizdoagaoctavio33@gmail.com>
2025-08-28 15:48:40 +0300

    Delete styles inside the Post.js file

--- src/Post.js
+++ src/Post.js
@@ -1,54 +1,7 @@
 class Post extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-      <style>
-        .content-card {
-          height: 300px;
-          width: auto;
-          position: relative;
-          scroll-snap-align: start;
-        }
-        .content-card>img:first-child {
-          object-fit: fill;
-          height: 300px;
-          width: auto;
-          border-radius: 4px;
-        }
-        .card-info {
-          height: 22px;
-          display: flex;
-          align-items: center;
-          flex-direction: row;
-          flex-wrap: nowrap;
-          position: absolute;
-          bottom: 0px;
-          right: 0px;
-          left: 0px;
-          padding: 5px;
-          background-color: var(--bg-light);
-          border-top: 3px solid var(--border);
-          gap: 5px;
-          border-bottom-left-radius: 4px;
-          border-bottom-right-radius: 4px;
-        }
-        .card-info img {
-          border-radius: 50%;
-        }
-        .card-info a {
-          text-decoration: none;
-          color: var(--text);
-          font-weight: 400;
-          font-size: 12px;
-          margin: 0;
-          padding: 0;
-        }
-        .card-info a:hover {
-          cursor: pointer;
-          color: var(--text-muted);
-        }
-      </style>
+    this.innerHTML = `
       <div class="content-card">
         <img id="post-image" width="300px" height="300px" loading="lazy" alt="Post"/>
         <div class="card-info">
@@ -59,9 +12,9 @@ class Post extends HTMLElement {
     `;
   }
   connectedCallback() {
-    this.shadowRoot.querySelector("#post-image").src = this.getAttribute("img1") || "../media/placeholder.webp";
-    this.shadowRoot.querySelector("#post-pfp").src = this.getAttribute("img2") || "../media/placeholder.webp";
-    this.shadowRoot.querySelector("a").textContent = `@${this.getAttribute("username")}` || "Username";
+    this.querySelector("#post-image").src = this.getAttribute("img1") || "../media/placeholder.webp";
+    this.querySelector("#post-pfp").src = this.getAttribute("img2") || "../media/placeholder.webp";
+    this.querySelector("a").textContent = `@${this.getAttribute("username")}` || "Username";
   }
 }
 

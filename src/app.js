54bf203f0f9bdd9fd3af3d7d24144c9adc2f8b83
octavio <vizdoagaoctavio33@gmail.com>
2025-09-04 19:36:29 +0300

    Make the mode buttons functional

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
 const lightModeBtn = document.getElementById("light-mode");
 const darkModeBtn = document.getElementById("dark-mode");
+const lightModeDropDown = document.getElementById('light-drop-down');
+const darkModeDropDown = document.getElementById('dark-drop-down');
+
+function toggleMode(lightBtn, darkBtn, isLight) {
+  lightBtn.classList.toggle("hidden");
+  darkBtn.classList.toggle("hidden");
+  isLight ? document.body.setAttribute("class", "light") : document.body.removeAttribute("class");
+}
+
+lightModeBtn.addEventListener("click", () => {
+  toggleMode(lightModeBtn, darkModeBtn, true);
+});
+darkModeBtn.addEventListener("click", () => {
+  toggleMode(lightModeBtn, darkModeBtn, false)
+});
+
+lightModeDropDown.addEventListener('click', () => {
+  toggleMode(lightModeDropDown, darkModeDropDown, true);
+});
+darkModeDropDown.addEventListener('click', () => {
+  toggleMode(lightModeDropDown, darkModeDropDown, false);
+})
 
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("drop-down-menu");
-
-lightModeBtn.addEventListener("click", () => {
-  document.body.setAttribute("class", "light");
-  darkModeBtn.classList.remove("hidden");
-  lightModeBtn.classList.add("hidden");
-});
-
-darkModeBtn.addEventListener("click", () => {
-  document.body.removeAttribute("class");
-  lightModeBtn.classList.remove("hidden");
-  darkModeBtn.classList.add("hidden");
-});
 
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("show");

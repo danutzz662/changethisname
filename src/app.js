b5f7b2b6738bcbd42a2ba2d758b32cf9972a4b6b
Danu <183900962+danutzz662@users.noreply.github.com>
2025-09-04 09:27:29 +0300

    Added functionality to the dropdown menu

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const lightModeBtn = document.getElementById('light-mode');
-const darkModeBtn = document.getElementById('dark-mode');
+const lightModeBtn = document.getElementById("light-mode");
+const darkModeBtn = document.getElementById("dark-mode");
 
-lightModeBtn.addEventListener('click', () => {
-  document.body.setAttribute('class', 'light');
-  darkModeBtn.classList.remove('hidden');
-  lightModeBtn.classList.add('hidden');
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("drop-down-menu");
+
+lightModeBtn.addEventListener("click", () => {
+  document.body.setAttribute("class", "light");
+  darkModeBtn.classList.remove("hidden");
+  lightModeBtn.classList.add("hidden");
 });
 
-darkModeBtn.addEventListener('click', () => {
-  document.body.removeAttribute('class');
-  lightModeBtn.classList.remove('hidden');
-  darkModeBtn.classList.add('hidden');
+darkModeBtn.addEventListener("click", () => {
+  document.body.removeAttribute("class");
+  lightModeBtn.classList.remove("hidden");
+  darkModeBtn.classList.add("hidden");
+});
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
 });
